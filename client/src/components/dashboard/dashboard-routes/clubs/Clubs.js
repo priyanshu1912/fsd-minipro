209802e323dc4161f6dbaf36a767fdbd7f0f1d75
Priyanshu <priyanshubhardwaj19dec@gmail.com>
2022-04-14 23:52:21 +0530

    changes

--- client/src/components/dashboard/dashboard-routes/clubs/Clubs.js
+++ client/src/components/dashboard/dashboard-routes/clubs/Clubs.js
@@ -1,14 +1,25 @@
 import React,{useState} from 'react'
 import './Clubs.css'
+import ExploreClubs from './ExploreClubs'
+import YourClubs from './YourClubs'
 
 function Clubs() {
     const [tab,setTab] = useState('your')
   return (
     <div className='clubs'>
-        <div style={{display:'flex',alignItems:'center'}}>
+        <div style={{display:'flex',alignItems:'center',color:'grey'}}>
             <div onClick={()=>setTab('your')} className={tab==='your'?'active-tab':'unactive-tab'}>Your clubs</div>
             <div onClick={()=>setTab('explore')} className={tab==='explore'?'active-tab':'unactive-tab'}>Explore clubs</div>
         </div>
+
+        {
+          tab==='your' &&
+          <YourClubs/>
+        }
+        {
+          tab==='explore' &&
+          <ExploreClubs/>
+        }
     </div>
   )
 }
