a6b3fef53c0557e7c822babcbd4fe21bb547ce7c
Gaurav Singh <sgaurav497@gmail.com>
2022-04-16 15:52:49 +0530

    Gaurav's 3rd commit

--- server/models/facultyModel.js
+++ server/models/facultyModel.js
@@ -4,25 +4,33 @@ import bcrypt from "bcrypt";
 const facultySchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true,
+        required: [true, 'Please enter a name'],
+        lowercase: true
     },
     username: {
         type: String,
-        required: true,
-        unique: true
+        required: [true, 'Please enter an username'],
+        unique: true,
+        lowercase: true
     },
     email: {
         type: String,
-        required: true,
-        unique: true
+        required: [true, 'Please enter an email'],
+        unique: true,
+        lowercase: true,
+        validate: [isEmail, 'Please enter a valid email']
     },
     program: {
         type: String,
-        required: true
+        required: [true, 'Please enter a program']
     },
     password: {
         type: String,
-        required: true
+        required: [true, 'Please enter a password'],
+        minlength: [6, 'Minimum password length is 6 characters']
+    },
+    profilePhoto: {
+        type: String
     },
     projects: {
         type: [String],
