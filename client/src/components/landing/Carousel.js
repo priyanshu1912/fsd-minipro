8ab593d3cb6e57797786309a4aa44e43dee8d631
Priyanshu <priyanshubhardwaj19dec@gmail.com>
2022-04-20 00:18:16 +0530

    changes

--- client/src/components/landing/Carousel.js
+++ client/src/components/landing/Carousel.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
 import './Carousel.css'
+import image1 from '../../assets/1.jpeg'
+import image2 from '../../assets/2.jpeg'
+import image3 from '../../assets/3.jpeg'
 
 export default class AutoPlay extends Component {
   render() {
@@ -11,25 +14,35 @@ export default class AutoPlay extends Component {
         autoplay: true,
         speed: 1000,
         autoplaySpeed: 5000,
-        cssEase: "linear"
+        cssEase: "linear",
+        fade: true,
       };
 
 
     return (
         <Slider {...settings}>
             <div>
-                <div className="slide-title">A central repository for all the clubs.</div>
-                <div className="slide-content">Student can join, leave, posts, get updates, on the club events, interact with other members, etc.</div>
+                <img src={image3} className='slide-image'/>
+                <div>
+                  <div className="slide-title">A central repository for all the clubs.</div>
+                  <div className="slide-content">Student can join, leave, posts, get updates, on the club events, interact with other members, etc.</div>
+                </div>
             </div>
 
             <div>
-                <div className="slide-title">A portal where Professors can post Projects.</div>
-                <div className="slide-content">And Students who are willing to work on those Projects can apply to work.</div>
+                <img src={image2} className='slide-image'/>
+                <div>
+                  <div className="slide-title">A portal where Professors can post Projects.</div>
+                  <div className="slide-content">And Students who are willing to work on those Projects can apply to work.</div>
+                </div>
             </div>
 
             <div>
-                <div className="slide-title">A Student Profile which shows affiliation to our University.</div>
-                <div className="slide-content">Also, Students can post their current projects, achievements, and their publications.</div>
+                <img src={image1} className='slide-image'/>
+                <div>
+                  <div className="slide-title">A Student Profile which shows affiliation to our University.</div>
+                  <div className="slide-content">Also, Students can post their current projects, achievements, and their publications.</div>
+                </div>
             </div>
         </Slider>
     );
